3c70103b54e299a453351270d01876e2a35a90eb
thiaguerd <mail@thiago.pro>
2020-01-27 14:31:54 -0500

    generate scaffold product

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Steps
 
 * rails new shop --skip-test
+
+* rails g scaffold product name:string quantity_in_stock:integer
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create name: 'Mouse', quantity_in_stock: 100
+Product.create name: 'Monitor', quantity_in_stock: 50
+Product.create name: 'Pencil', quantity_in_stock: 40
+Product.create name: 'Glass', quantity_in_stock: 10
